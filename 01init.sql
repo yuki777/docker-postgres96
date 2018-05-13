49975be9034eedfda80635052fa72144bd3b034a
Yuki Adachi <yuki777@gmail.com>
2018-05-13 14:20:08 +0700

    Delete init.sql

--- 01init.sql
+++
@@ -1,3 +0,0 @@
-create role dockeruser login password 'dockerpass';
-create database dockerdb;
-grant all privileges on database dockerdb to dockeruser;
--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,1 @@
 FROM postgres:9.6-alpine
-COPY *.sql /docker-entrypoint-initdb.d/
